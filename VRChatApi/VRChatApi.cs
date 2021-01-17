6cc4fe6a2a3c9102617ce9caa646552975acb3f5
Sia819 <jjn970819@gmail.com>
2021-01-17 20:41:28 +0900

    0.0.0
    Making...

---
+++ VRChatApi/VRChatApi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using VRChatApi.Endpoints;
+using VRChatApi.Logging;
+
+namespace VRChatApi
+{
+    public class VRChatApi
+    {
+        private static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
+
+        public RemoteConfig RemoteConfig { get; set; }
+        public UserApi UserApi { get; set; }
+        public FriendsApi FriendsApi { get; set; }
+        public WorldApi WorldApi { get; set; }
+        public ModerationsApi ModerationsApi { get; set; }
+        public AvatarApi AvatarApi { get; set; }
+        public FavouriteApi FavouriteApi { get; set; }
+
+        public VRChatApi(string username, string password)
+        {
+            Logger.Trace(() => $"Entering {nameof(VRChatApi)} constructor");                            /// Log : VRChatApi 클래스 생성자의 진입점입니다.
+            Logger.Debug(() => $"Using username {username}");                                           /// Log : VRChat ID는 {username} 입니다.
+
+            /// initialize endpoint classes // API endpoint 클래스들을 초기화하여 새로 생성합니다.
+            RemoteConfig = new RemoteConfig();
+            UserApi = new UserApi(username, password);
+            FriendsApi = new FriendsApi();
+            WorldApi = new WorldApi();
+            ModerationsApi = new ModerationsApi();
+            AvatarApi = new AvatarApi();
+            FavouriteApi = new FavouriteApi();
+
+            /// initialize http client // static class인 Global의, HttpClient멤버가 처음 사용될 때, 이 멤버를 초기화합니다.
+            // TODO: use the auth cookie
+            if (Global.HttpClient == null)
+            {
+                Logger.Trace(() => $"Instantiating {nameof(HttpClient)}");                              /// Log : Global.HttpClient 인스턴스 생성 중
+                Global.HttpClient = new HttpClient();
+                Global.HttpClient.BaseAddress = new Uri("https://api.vrchat.cloud/api/1/");
+                Logger.Info(() => $"VRChat API base address set to {Global.HttpClient.BaseAddress}");   /// Log : VRChat의 기본 API 주소는 {Global.HttpClient.BaseAddress} 로 설정되었습니다.
+            }
+
+            /// api요청으로 응답대상 username, password를 해당사이트에서 지원하는 형식인 Base64방식으로 인코딩합니다.
+            string authEncoded = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{UserApi.Username}:{UserApi.Password}"));
+
+            /// 편의성을 위한 임시변수입니다. class 변수는 참조형이므로 이 경우,
+            /// 참고, header가 수정되면 static변수인 Global.HttpClient.DefaultRequestHeaders도 같이 수정됩니다.
+            System.Net.Http.Headers.HttpRequestHeaders requestHeader = Global.HttpClient.DefaultRequestHeaders;
+
+            if (requestHeader.Contains("Authorization"))
+            {
+                Logger.Debug(() => "Removing existing Authorization header");
+                requestHeader.Remove("Authorization");
+            }
+
+            /// Global.HttpClient의 DefaultRequestHeaders에 ID / PW를 추가합니다.
+            /// static변수에 적용함으로써, HttpRequest의 요청으로 ID와 PW를 전송하는데에 사용됩니다.
+            requestHeader.Add("Authorization", $"Basic {authEncoded}");
+
+            Logger.Trace(() => $"Added new Authorization header");                                      /// Log : Global.HttpClient에 권한부여 헤더가 추가되었습니다.
+        }
+    }
+}
---
+++ VRChatFriends/Common/Example.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VRChatFriends.Common;
+using VRChatApi;
+using VRChatApi.Classes;
+
+namespace VRChatFriends
+{
+    class ApiExample
+    {
+        static async Task Main(string[] args)
+        {
+            VRChatApi.VRChatApi api = new VRChatApi.VRChatApi("lunasia819@gmail.com", "tjdrb1458");
+            VRChatApi.Logging.LogProvider.SetCurrentLogProvider(new ColoredConsoleLogProvider());
+
+            /// Remote Config
+            /// VRChatApi 인스턴스의, 프로퍼티 크래스.RemoteConfig class 프로퍼티의 Get함수를 호출합니다.
+            /// VRChatApi.Global.ApiKey를 서버에서 받은 값으로 업데이트 합니다.
+            /// 이는, public ApiKey가 만료되었을때, 서버에서 새 ApiKey를 받는것을 기대할 수 있습니다.
+            ConfigResponse config = await api.RemoteConfig.Get();
+
+            #region UserAPI
+            /// 회원가입 API로 새 계정만들기
+            // UserResponse userNew = await api.UserApi.Register("someName", "somePassword", "some@email.com");
+            /// 로그인하기
+            UserResponse user = await api.UserApi.Login();
+            /// 유저정보 수정
+            //UserResponse userUpdated = await api.UserApi.UpdateInfo(user.id, null, null, null, new List<string>() { "admin_moderator", "admin_scripting_access", "system_avatar_access", "system_world_access" });
+            #endregion
+
+            #region Friends
+            // 친구 리스트 불러오기
+            List<UserBriefResponse> friends = await api.FriendsApi.Get(0, 20, true);
+            // 친구 신청요청
+            //NotificationResponse friendRequestResponse = await api.FriendsApi.SendRequest("usr_f8220fc0-e6f9-45ab-8d9f-ae00e8491685", api.UserApi.Username);
+            // 친구 삭제요청
+            //string friendDeletionResponse = await api.FriendsApi.DeleteFriend("usr_f8220fc0-e6f9-45ab-8d9f-ae00e8491685");
+            // 친구 승락요청
+            //await api.FriendsApi.AcceptFriend("usr_f8220fc0-e6f9-45ab-8d9f-ae00e8491685");
+            #endregion
+
+            #region WorldAPI
+            // * var worldId = "wrld_b2d24c29-1ded-4990-a90d-dd6dcc440300"; // The great Pug
+            // * WorldResponse world = await api.WorldApi.Get(worldId);
+            //List<WorldBriefResponse> starWorlds = await api.WorldApi.Search(WorldGroups.Favorite, count: 4);
+            // * List<WorldBriefResponse> scaryWorlds = await api.WorldApi.Search(keyword: "Scary", sort: SortOptions.Popularity);
+            // * List<WorldBriefResponse> featuredWorlds = await api.WorldApi.Search(featured: true);
+            // * WorldMetadataResponse metadata = await api.WorldApi.GetMetadata(worldId);
+            // * var instances = new List<WorldInstanceResponse>();
+            // * if (world.instances.Count > 0) {
+            // *     foreach(WorldInstance instance in world.instances)
+            // *     {
+            // *         WorldInstanceResponse worldInst = await api.WorldApi.GetInstance(world.id, instance.id);
+            // *         instances.Add(worldInst);
+            // *     }
+            // * }
+            #endregion
+
+            // avatar api
+            // * AvatarResponse avatar = await api.AvatarApi.GetById("avtr_17036f54-f706-46bf-8a43-0c60564123ff");
+
+            Console.ReadLine();
+        }
+    }
+}
